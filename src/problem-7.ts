edcc03ae0b5b59b1ffb56d1c67ea1bd0540b1be1
S H-SuHan <suhanur35-448@diu.edu.bd>
2024-05-07 20:31:31 +0600

    Problem 7 solved Here

---
+++ src/problem-7.ts
@@ -0,0 +1,14 @@
+{
+    const logString = (params: unknown): void =>{
+        if (typeof params === "string") {
+            console.log(params);
+        } else {
+            console.log("Input is not a string.");
+        }
+    }
+
+    logString("Hello, TypeScript!");
+    logString(42);
+
+
+}
