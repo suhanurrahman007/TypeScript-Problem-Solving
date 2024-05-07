a254c74fe8ae48bad9aa3d8d4eec64601c7eeb49
S H-SuHan <suhanur35-448@diu.edu.bd>
2024-05-07 20:09:57 +0600

    Problem 5 solved Here

---
+++ src/problem-5.ts
@@ -0,0 +1,24 @@
+interface Student {
+  name: string;
+  age: number;
+  grades: number[];
+};
+
+const student1: Student = {
+  name: "Bob",
+  age: 17,
+  grades: [75, 80, 82, 88, 90],
+};
+
+const calculateAverageGrade = (student: Student): number => {
+
+  const totalGrade = student.grades.reduce((acc, grade) => acc + grade, 0);
+  const averageGrade = totalGrade / student.grades.length;
+
+  return averageGrade;
+
+};
+
+const averageGradeForBob = calculateAverageGrade(student1);
+
+console.log(averageGradeForBob);
